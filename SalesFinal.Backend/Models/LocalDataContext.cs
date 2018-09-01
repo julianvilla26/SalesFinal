c15e1813df9776e6bda787566a08790a752e01ac
Julian <julianvilla30@gmail.com>
2018-09-01 14:55:34 -0500

    Projects, creacion de apis, contrllers, dataconection y publish en azure

---
+++ SalesFinal.Backend/Models/LocalDataContext.cs
@@ -0,0 +1,10 @@
+﻿
+namespace SalesFinal.Backend.Models
+{
+    using Domain.Models;
+
+    public class LocalDataContext : DataContext
+    {
+        public System.Data.Entity.DbSet<SalesFinal.Common.Models.Product> Products { get; set; }
+    }
+}
---
+++ SalesFinal.Domain/Models/DataContext.cs
@@ -0,0 +1,13 @@
+﻿namespace SalesFinal.Domain.Models
+{
+    using System.Data.Entity;
+
+    public class DataContext : DbContext
+    {
+        public DataContext() : base("DefaultConnection")
+        {
+        }
+
+        public System.Data.Entity.DbSet<SalesFinal.Common.Models.Product> Products { get; set; }
+    }
+}
